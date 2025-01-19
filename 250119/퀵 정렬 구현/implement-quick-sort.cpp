02287a25644ab18d180a146cf1313cd0690c0bc3
Nam Yoonchang <144316654+ycnham@users.noreply.github.com>
2025-01-19 14:06:51 +0900

    Title: 퀵 정렬 구현, Time: 273ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 250119/퀵 정렬 구현/implement-quick-sort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 100000
+
+using namespace std;
+
+int n;
+int arr[MAX_N];
+// int arr[100000];
+
+int Partition(int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for(int j = low; j < high; j++)
+    {
+        if(arr[j] < pivot)
+        {
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[high]);
+
+    return i + 1;
+}
+
+void QuickSort(int low, int high)
+{
+    if(low, high)
+    {
+        int pos = Partition(low, high);
+
+        QuickSort(low, pos - 1);
+        QuickSort(pos + 1, high);
+    }
+}
+
+int main()
+{
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    QuickSort(0, n - 1);
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
