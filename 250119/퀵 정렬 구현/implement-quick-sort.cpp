9c5ff3e7f20e8a66f194ce4acb5ee34928f600f2
Nam Yoonchang <144316654+ycnham@users.noreply.github.com>
2025-01-19 14:22:16 +0900

    Title: 퀵 정렬 구현, Time: 34ms, Memory: 1MB, Status: Passed - CodeTree

--- 250119/퀵 정렬 구현/implement-quick-sort.cpp
+++ 250119/퀵 정렬 구현/implement-quick-sort.cpp
@@ -7,21 +7,22 @@ using namespace std;
 
 int n;
 int arr[MAX_N];
-// int arr[100000];
 
 int Partition(int low, int high)
 {
-    int pivot = arr[high];
-    int i = low - 1;
+    int pivot = arr[high];             
+    int i = low - 1;                   
+ 
 
     for(int j = low; j < high; j++)
-    {
-        if(arr[j] < pivot)
         {
-            swap(arr[i], arr[j]);
+            if(arr[j] < pivot)
+            {
+                i++;
+                swap(arr[i], arr[j]);
+            }
         }
-    }
-
+    
     swap(arr[i + 1], arr[high]);
 
     return i + 1;
@@ -29,11 +30,10 @@ int Partition(int low, int high)
 
 void QuickSort(int low, int high)
 {
-    if(low, high)
+    if(low < high)
     {
         int pos = Partition(low, high);
-
-        QuickSort(low, pos - 1);
+        QuickSort(low, pos - 1); 
         QuickSort(pos + 1, high);
     }
 }
@@ -42,11 +42,11 @@ int main()
 {
     cin >> n;
 
-    for (int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-
+    
     QuickSort(0, n - 1);
 
     for(int i = 0; i < n; i++)
